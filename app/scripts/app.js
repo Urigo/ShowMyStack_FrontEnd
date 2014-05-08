c7cb8f19b33edad8a7a9d375ce8689e26affc4f7
dotansimha <dotansimha@gmail.com>
2014-05-08 19:10:39 +0300

    Initial commit with yeoman structure

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var showMyStackApp = angular
+    .module('showMyStackApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+showMyStackApp
+    .controller('MainCtrl', function($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
